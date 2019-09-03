bc9d4b81cf9a7d4183cf4fab01854a063b771503
Robert Mosolgo <rdmosolgo@gmail.com>
2019-09-03 09:43:20 -0400

    Move initializer code to schema file

--- app/graphql/blog_schema.rb
+++ app/graphql/blog_schema.rb
@@ -2,3 +2,9 @@ class BlogSchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
 end
+
+GraphQL::Errors.configure(BlogSchema) do
+  rescue_from GraphQL::UnauthorizedError do |e|
+    GraphQL::ExecutionError.new e.message
+  end
+end
--- config/initializers/graphql_errors.rb
+++
@@ -1,5 +0,0 @@
-GraphQL::Errors.configure(BlogSchema) do
-  rescue_from GraphQL::UnauthorizedError do |e|
-    GraphQL::ExecutionError.new e.message
-  end
-end
